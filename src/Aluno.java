ddd03d67b96484dd1ace4f5ffd942f4891640ca9
Lucas Peixoto Rodrigues <lucaspeixotorodrigues@gmail.com>
2025-05-15 23:51:35 -0300

    Teste 1

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,3 +1,20 @@
 public class Aluno {
+    private String nome;
+    private int matricula;
+    private String cursoDeGraduação;
 
+    public void setAluno(String nome, int matricula, String cursoDeGraduação){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cursoDeGraduação = cursoDeGraduação;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+        public String getCursoDeGraduação() {
+        return cursoDeGraduação;
+    }
 }
--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean rodagem = true;
+        Scanner scanner = new Scanner(System.in);
+        String modo;
+
+        while (rodagem) {
+            System.out.println("Seja bem vindo ao SIGAA 2.0! Em qual modo você gostaria de operar?");
+            System.out.println("1 - Modo Aluno");
+            System.out.println("2 - Modo Disciplina");
+            System.out.println("3 - Modo Avaliação");
+            System.out.println("0 - Sair");
+            modo = scanner.nextLine();
+
+            if (modo != "0") {
+                
+            }
+            else {
+                break;
+            }
+        }
     }
 }
